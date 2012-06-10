ba018e96238d780a4cec20bec45af08125b41258
James Smith <james@loopj.com>
2012-06-10 12:53:18 -0700

    Cleaned up and documented the plugin arch

--- notification-plugin.js
+++ notification-plugin.js
@@ -1,28 +1,63 @@
-var sugar = require("sugar");
-var request = require("superagent");
-var fs = require("fs");
-var path = require("path");
+require("sugar");
 
-var NotificationPlugin = (function() {
+//
+// The base Bugsnag NotificationPlugin class
+// Extend this class to create your own Bugsnag notification plugins:
+// 
+// JavaScript:
+//   var util = require("util");
+//   var NotificationPlugin = require("../../notification-plugin.js")
+//
+//   function MyPlugin() {}
+//   util.inherits(MyPlugin, NotificationPlugin);
+//   MyPlugin.receiveEvent = function (config, reason, project, error) {
+//     ...
+//   };
+//   module.exports = MyPlugin;
+//
+// CoffeeScript:
+//   NotificationPlugin = require "../../notification-plugin.js"
+//   class MyPlugin extends NotificationPlugin
+//     @receiveEvent = (config, reason, project, error) ->
+//       ...
+//   module.exports = MyPlugin
+//
+// All notification plugins must override the receiveEvent function to perform
+// the notification. This method is fired when a new event is triggered.
+//
+
+var NotificationPlugin = (function () {
   function NotificationPlugin() {}
 
-  NotificationPlugin.shortMessage = function(reason, projectName, error) {
+  // Fired when a new event is triggered for notification
+  NotificationPlugin.receiveEvent = function (config, reason, project, error) {
+    throw new Error("Plugins must override receiveEvent");
+  };
+
+
+  // Build a short message from this event
+  NotificationPlugin.shortMessage = function (reason, projectName, error) {
     return "[" + reason + " on " + projectName + "] " + error.exceptionClass + ": " + (error.message.substr(0, 10));
   };
 
-  NotificationPlugin.httpGet = function(url, params, callback) {
+
+  // Utility methods for http requests
+  var request = require("superagent");
+  NotificationPlugin.httpGet = function (url, params, callback) {
     return request.get(url).send(params).end(callback);
   };
 
-  NotificationPlugin.httpPost = function(url, params, callback) {
+  NotificationPlugin.httpPost = function (url, params, callback) {
     return request.post(url).send(params).type("form").end(callback);
   };
 
-  NotificationPlugin.httpPostJson = function(url, obj, callback) {
+  NotificationPlugin.httpPostJson = function (url, obj, callback) {
     return request.post(url).send(obj).end(callback);
   };
 
-  NotificationPlugin.fireTestEvent = function(config) {
+
+  // Fire a test event to your notification plugin (do not override)
+  NotificationPlugin.fireTestEvent = function (config) {
     var error, projectName, reason;
     reason = "First exception";
     projectName = "Example";
@@ -39,51 +74,53 @@ var NotificationPlugin = (function() {
       "firstReceived": new Date(),
       "eventUrl": "http://www.bugsnag.com/blah"
     };
-    return NotificationPlugin.receiveEvent(config, reason, projectName, error);
+    return this.receiveEvent(config, reason, projectName, error);
   };
 
-  NotificationPlugin.validateConfig = function(config, pluginConfigFile) {
+
+  // Configuration validation methods (do not override)
+  var configError = function (message) {
+    throw new Error("ConfigurationError: " + message);
+  };
+
+  NotificationPlugin.validateConfig = function (config, pluginConfigFile) {
+    var fs = require("fs");
     var pluginConfig = JSON.parse(fs.readFileSync(pluginConfigFile, "ascii"));
     pluginConfig.options.each(function (option) {
       var configValue = config[option.name];
 
       // Validate all non-optional config options are present
       if (!(configValue || option.optional)) {
-        NotificationPlugin.configError("Missing '" + option.name + "'");
+        configError("Missing '" + option.name + "'");
       }
 
       // Validate fields with allowed values
       if (configValue && option.allowedValues && option.allowedValues.none(configValue)) {
-        NotificationPlugin.configError("Invalid value for '" + option.name + "'");
+        configError("Invalid value for '" + option.name + "'");
       }
     });
   };
 
-  NotificationPlugin.configError = function(message) {
-    throw new Error("ConfigurationError: " + message);
-  };
-
-  NotificationPlugin.receiveEvent = function(reason, projectName, error) {
-    throw new Error("Plugins must override receiveEvent");
-  };
 
   return NotificationPlugin;
 })();
 module.exports = NotificationPlugin;
 
 
+
 // If running plugins from the command line, allow them to fire test events
 if (module.parent && module.parent.parent === null) {
+  var path = require("path");
   var argv = require("optimist").argv;
-  var flags = Object.keys(argv).exclude("_", "$0");
-  if (flags.length > 0) {
-    var config = {};
-    flags.each(function (flag) {
-      config[flag] = argv[flag];
-    });
 
-    NotificationPlugin.validateConfig(config, "" + (path.dirname(module.parent.filename)) + "/config.json");
-    var plugin = require(module.parent.filename);
-    plugin.fireTestEvent(config);
-  }
+  var flags = Object.keys(argv).exclude("_", "$0");
+  var config = {};
+  flags.each(function (flag) {
+    config[flag] = argv[flag];
+  });
+
+  NotificationPlugin.validateConfig(config, path.dirname(module.parent.filename) + "/config.json");
+
+  var plugin = require(module.parent.filename);
+  plugin.fireTestEvent(config);
 }
